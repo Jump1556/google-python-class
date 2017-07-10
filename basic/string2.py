9368975897e5ef4f3eec6c8a7f39221e166c8490
Anastasiia Kostashenko <anastasiia.kostashenko@gmail.com>
2017-07-10 17:56:45 +0200

    third pert done string2

--- basic/string2.py
+++ basic/string2.py
@@ -48,8 +48,15 @@ def not_bad(s):
 # Given 2 strings, a and b, return a string of the form
 #  a-front + b-front + a-back + b-back
 def front_back(a, b):
-  # +++your code here+++
-  return
+  if len(a)%2 == 0:
+    a_front, a_back = a[:len(a)/2], a[len(a)/2:]
+  else:
+    a_front, a_back = a[:(len(a)+1)/2], a[(len(a)+1)/2:]
+  if len(b)%2 == 0:
+    b_front, b_back = b[:len(b)/2], b[len(b)/2:]
+  else:
+    b_front, b_back = b[:(len(b)+1)/2], b[(len(b)+1)/2:]
+  return a_front + b_front + a_back + b_back
 
 
 # Simple provided test() function used in main() to print
--- task23.py
+++ task23.py
@@ -7,6 +7,29 @@ __author__ = 'anastasia'
 # So 'This dinner is not that bad!' yields:
 # This dinner is good!
 
-s.find('not')
-s.find('bad')
-s.replace()
+#s.find('not')
+#s.find('bad')
+#s.replace()
+
+a ='abcd'
+b ='xy'
+# abxcdy
+def front_back(a, b):
+  if len(a)%2 == 0 or len(b)%2 == 0:
+    a_front, a_back = a[:len(a)/2], a[len(a)/2:]
+    b_front, b_back = b[:len(b)/2], b[len(b)/2:]
+  else:
+    a_front, a_back = a[:(len(a)+1)/2], a[(len(a)+1)/2:]
+    b_front, b_back = b[:(len(b)+1)/2], b[(len(b)+1)/2:]
+  return a_front + b_front + a_back + b_back
+print (main)
+def front_back(a, b):
+  if len(a)%2 == 0:
+    a_front, a_back = a[:len(a)/2], a[len(a)/2:]
+  else:
+    a_front, a_back = a[:(len(a)+1)/2], a[(len(a)+1)/2:]
+  if len(b)%2 == 0:
+    b_front, b_back = b[:len(b)/2], b[len(b)/2:]
+  else:
+    b_front, b_back = b[:(len(b)+1)/2], b[(len(b)+1)/2:]
+  return a_front + b_front + a_back + b_back
